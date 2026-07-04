0b4d5710182cf6193fdfbf019a6f19b3d668912b
Ardiansah040505 <ardiansahrohman356@gmail.com>
2026-07-04 17:53:22 +0700

    try deploy3

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://eclypse-production.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
